4613d9f3d68dd856dedde88d0af2ff5b42acbf70
jathosn <thoresen.jan.94@gmail.com>
2024-09-08 15:25:13 +0200

    player name placeholder in Main removed.

--- Refactoring/Program.cs
+++ Refactoring/Program.cs
@@ -14,11 +14,7 @@ namespace Refactoring
             StandardMessages.WelcomeMessage();
             PlayerData.PlayerCount();
             PlayerData.PlayerName();
-            foreach (var player in PlayerData.players)
-            {
-                Console.WriteLine($"Player Name: {player.Name}");
-            }
-            Console.WriteLine("\nPress any key to exit...");
+            StandardMessages.GameStarting();
             Console.ReadKey();  // This will keep the console window open until a key is pressed
         }
     }
