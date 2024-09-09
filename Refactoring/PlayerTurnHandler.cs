b9a01d5ffc8d04668379672b22a67161f5f18bea
Jathosn <thoresen.jan.94@gmail.com>
2024-09-09 12:42:23 +0200

    no longer initiates throws on its own on turn 1

--- Refactoring/PlayerTurnHandler.cs
+++ Refactoring/PlayerTurnHandler.cs
@@ -30,27 +30,34 @@ namespace Refactoring
                 Console.WriteLine($"\u001b[38;2;255;150;0m\nYou have {_player.AvailableThrows} throw(s) available.\u001b[0m \n");
                 Console.WriteLine("Press 'ENTER' to throw\nPress 'S' to view scoreboard\nPress 'E' to end turn\nPress 'H' to view shorthand notations");
 
-
                 while (!decisionMade)
-
                 {
                     bool turnSkipped = false;
-
                     var keyPress = Console.ReadKey(true).Key;
+
                     switch (keyPress)
                     {
                         case ConsoleKey.Enter:
                             Console.Clear();
-                            decisionMade = true;
+                            decisionMade = true;  // Proceed to dice rolling
                             break;
+
                         case ConsoleKey.S:
                             Console.Clear();
                             ViewScoreboard();
+                            Console.WriteLine($"\u001b[38;2;255;150;0m\nYou have {_player.AvailableThrows} throw(s) available.\u001b[0m \n");
+                            Console.WriteLine("Press 'ENTER' to throw\nPress 'S' to view scoreboard\nPress 'E' to end turn\nPress 'H' to view shorthand notations");
+                            decisionMade = false;  // Stay in the input loop
                             break;
+
                         case ConsoleKey.H:
                             Console.Clear();
                             Shortcuts.DisplayShorthandNotations();
+                            Console.WriteLine($"\u001b[38;2;255;150;0m\nYou have {_player.AvailableThrows} throw(s) available.\u001b[0m \n");
+                            Console.WriteLine("Press 'ENTER' to throw\nPress 'S' to view scoreboard\nPress 'E' to end turn\nPress 'H' to view shorthand notations");
+                            decisionMade = false;  // Stay in the input loop
                             break;
+
                         case ConsoleKey.E:
                             Console.Clear();
                             EndTurnEarly();
@@ -58,106 +65,101 @@ namespace Refactoring
                             turnSkipped = true;
                             break;
                     }
+
                     if (turnSkipped)
                     {
                         continue;  // Skip to the next player if the turn was skipped
                     }
-                    int currentThrows = _player.AvailableThrows;
-                    DiceThrower diceThrower = new DiceThrower();
-                    int throwCount = _player.AvailableThrows;
 
-                    bool endTurn = false;
-                    for (int i = 0; i < throwCount; i++)
+                    if (decisionMade)  // Only proceed to dice rolling if Enter was pressed
                     {
+                        int currentThrows = _player.AvailableThrows;
+                        DiceThrower diceThrower = new DiceThrower();
+                        int throwCount = _player.AvailableThrows;
+                        bool endTurn = false;
 
+                        for (int i = 0; i < throwCount; i++)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine($"{diceThrower.GetDiceValuesAsString()}\n");
+                            Console.ResetColor();
+
+                            int throwsRemaining = throwCount - i - 1;
+                            if (throwsRemaining > 0)
+                            {
+                                Console.WriteLine($"\u001b[38;2;255;150;0mYou have {throwsRemaining} throw(s) remaining, {_player.Name}\u001b[0m \n");
+                                decisionMade = false;
+                                Console.WriteLine("Press 'ENTER' to continue");
+                                Console.WriteLine("Press 'S' to view scoreboard");
+                                Console.WriteLine("Press 'E' to end turn");
+                                Console.WriteLine("Press 'H' to view shorthand notations");
+
+                                while (!decisionMade)
+                                {
+                                    keyPress = Console.ReadKey(true).Key;
+
+                                    if (keyPress == ConsoleKey.S)
+                                    {
+                                        Console.Clear();
+                                        ViewScoreboard();
+                                        Console.WriteLine($"\u001b[38;2;255;150;0m\nYou have {_player.AvailableThrows} throw(s) available.\u001b[0m \n");
+                                        Console.WriteLine("Press 'ENTER' to throw\nPress 'S' to view scoreboard\nPress 'E' to end turn\nPress 'H' to view shorthand notations");
+                                        decisionMade = false;  // Stay in the input loop
+                                    }
+                                    else if (keyPress == ConsoleKey.H)
+                                    {
+                                        Console.Clear();
+                                        Shortcuts.DisplayShorthandNotations();
+                                        Console.WriteLine($"\u001b[38;2;255;150;0m\nYou have {_player.AvailableThrows} throw(s) available.\u001b[0m \n");
+                                        Console.WriteLine("Press 'ENTER' to throw\nPress 'S' to view scoreboard\nPress 'E' to end turn\nPress 'H' to view shorthand notations");
+                                        decisionMade = false;  // Stay in the input loop
+                                    }
+                                    else if (keyPress == ConsoleKey.E)
+                                    {
+                                        Console.Clear();
+                                        _player.AvailableThrows = 3 + throwsRemaining;
+                                        Console.ForegroundColor = ConsoleColor.Green;
+                                        Console.WriteLine($"You ended your turn early. {throwsRemaining} throws carried over to your next turn.\n");
+                                        Console.ResetColor();
+                                        decisionMade = true;
+                                        endTurn = true;  // Set flag to true to indicate turn end
+                                        break;
+                                    }
+                                    else if (keyPress == ConsoleKey.Enter)
+                                    {
+                                        decisionMade = true;
+                                    }
+                                }
+
+                                if (endTurn)
+                                {
+                                    break;
+                                }
+
+                                // Continue rolling dice
+                                int[] currentRoll = diceThrower.DiceValues;  // Get the current roll
+                                bool[] diceToKeep = diceThrower.GetDiceToKeep(currentRoll);  // Ask player which dice to keep
+                                diceThrower.RollSpecificDice(diceToKeep);
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("No throws remaining.");
+                            }
+                        }
+
+                        Console.Clear();
                         Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine($"{diceThrower.GetDiceValuesAsString()}\n");
+                        Console.WriteLine("Result:");
+                        Console.WriteLine($"\n{diceThrower.GetDiceValuesAsString()}\n");
                         Console.ResetColor();
-
-                        int throwsRemaining = throwCount - i - 1;
-                        if (throwsRemaining > 0)
-                        {
-                            Console.WriteLine($"\u001b[38;2;255;150;0mYou have {throwsRemaining} throw(s) remaining, {_player.Name}\u001b[0m \n");
-                            decisionMade = false;
-                            Console.WriteLine("Press 'ENTER' to continue");
-                            Console.WriteLine("Press 'S' to view scoreboard");
-                            Console.WriteLine("Press 'E' to end turn");
-                            Console.WriteLine("Press 'H' to view shorthand notations");
-                            while (!decisionMade)
-                            {
-
-                                keyPress = Console.ReadKey(true).Key;
-
-                                if (keyPress == ConsoleKey.S)
-                                {
-                                    Console.Clear();
-                                    // Display the scoreboard and re-prompt the player
-                                    PlayerData.PrintPlayerCard(_player);
-                                    Console.ForegroundColor = ConsoleColor.White;
-                                    Console.WriteLine($"\n{diceThrower.GetDiceValuesAsString()}\n");
-                                    Console.WriteLine($"\u001b[38;2;255;150;0mYou have {throwsRemaining} throw(s) remaining, {_player.Name} \u001b[0m \n");
-                                    Console.WriteLine($"Press 'ENTER' to throw\nPress 'S' to view scoreboard \nPress 'E' to end turn \nPress 'H' to view shorthand notations");
-                                }
-                                else if (keyPress == ConsoleKey.H)
-                                {
-                                    Console.Clear();
-                                    // Display shorthand notations when 'H' is pressed
-                                    Shortcuts.DisplayShorthandNotations();
-                                    Console.ForegroundColor = ConsoleColor.White;
-                                    Console.WriteLine($"{diceThrower.GetDiceValuesAsString()}\n");
-                                    Console.WriteLine($"\u001b[38;2;255;150;0mYou have {throwsRemaining} throw(s) remaining, {_player.Name}\u001b[0m \n");
-                                    Console.WriteLine($"Press 'ENTER' to throw\nPress 'S' to view scoreboard \nPress 'E' to end turn \nPress 'H' to view shorthand notations");
-
-                                }
-                                else if (keyPress == ConsoleKey.E)
-                                {
-                                    Console.Clear();
-                                    // End the player's turn and add remaining throws to next turn
-                                    _player.AvailableThrows = 3 + throwsRemaining;
-                                    Console.ForegroundColor = ConsoleColor.Green;
-                                    Console.WriteLine($"You ended your turn early. {throwsRemaining} throws carried over to your next turn.\n");
-                                    Console.ResetColor();
-                                    Console.WriteLine($"Press 'ENTER' to throw\nPress 'S' to view scoreboard \nPress 'E' to end turn \nPress 'H' to view shorthand notations");
-
-
-                                    decisionMade = true;
-                                    endTurn = true;  // Set flag to true to indicate turn end
-                                    break;  // Exit the loop
-                                }
-                                else if (keyPress == ConsoleKey.Enter)
-                                {
-                                    decisionMade = true;
-                                }
-
-                            }
-                            if (endTurn)
-                            {
-                                break;
-                            }
-
-                            int[] currentRoll = diceThrower.DiceValues;       // Get the current roll
-                            bool[] diceToKeep = diceThrower.GetDiceToKeep(currentRoll); // Ask player which dice to keep
-                            diceThrower.RollSpecificDice(diceToKeep);
-                        }
-                        else
-                        {
-                            Console.Clear();
-                            Console.WriteLine("No throws remaining.");
-                        }
+                        _player.ChooseScoreCategory(diceThrower.DiceValues);
+                        Console.WriteLine();
                     }
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("Result:");
-                    Console.WriteLine($"\n{diceThrower.GetDiceValuesAsString()}\n");
-                    Console.ResetColor();
-                    _player.ChooseScoreCategory(diceThrower.DiceValues);
-
-                    Console.WriteLine();
                 }
-
             }
         }
-                
+
 
         private void RollDice()
         {
@@ -183,8 +185,6 @@ namespace Refactoring
             Console.Clear();
             Console.WriteLine($"{_player.Name}'s Scoreboard:");
             PlayerData.PrintPlayerCard(_player);
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
         }
 
         private void EndTurnEarly()
