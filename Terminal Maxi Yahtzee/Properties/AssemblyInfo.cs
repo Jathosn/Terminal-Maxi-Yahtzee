4a87e4c37416c5c82de8b760df60bb63f529d849
Jathosn <thoresen.jan.94@gmail.com>
2024-09-16 10:19:46 +0200

    Old project source deprecated. refactored code renamed to match that of the old source project

--- Terminal Maxi Yahtzee/Properties/AssemblyInfo.cs
+++ Terminal Maxi Yahtzee/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Terminal Maxi Yahtzee")]
+[assembly: AssemblyTitle("Refactoring")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Terminal Maxi Yahtzee")]
+[assembly: AssemblyProduct("Refactoring")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b267bb5d-8315-4776-bd07-4c1ad769170f")]
+[assembly: Guid("bd2f44c3-7c4b-4d9d-9e28-b45d7884ce9a")]
 
 // Version information for an assembly consists of the following four values:
 //
